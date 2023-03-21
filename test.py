73f808eefd6c07d0b1a412c56bbb553b11a6a706
vishesh1470 <127332329+vishesh1470@users.noreply.github.com>
2023-03-21 16:24:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-# -*- coding: UTF-8 -*-
 import requests as req
 import json,sys,time
-#先注册azure应用,确保应用有以下权限:
-#files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
-#user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
-#mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
+
+#files:	Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
+#user:	User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
+#mail:  Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
 
 
 
@@ -45,38 +43,38 @@ def main():
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print("1调用成功"+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
             num1+=1
-            print("2调用成功"+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print('3调用成功'+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
             num1+=1
-            print('4调用成功'+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')    
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')    
+            print("Successful Call : " + str(num1))  
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
-            print('7调用成功'+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次') 
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
-            print('9调用成功'+str(num1)+'次')
+            print("Successful Call : " + str(num1))
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
-            print('10调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
+            print("Successful Call : " + str(num1))
+            print('Completed :', localtime)
     except:
         print("pass")
         pass
